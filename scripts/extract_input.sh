5fbd4d9ff1e5689aab91772368abf1670465348e
ZheRao <zheraoeminem@gmail.com>
2025-11-06 16:53:00 -0600

    experimenting

---
+++ scripts/extract_input.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+win_token_path="$(wslpath -u 'c:/Users/ZheRao/OneDrive - Monette Farms/Monette Farms Team Site - Innovation Projects/Production/Database/Bronze/Authentication/QBO/')"
+wsl_token_path=~/projects/ETLPipeline/Database/.inputs/dev/
